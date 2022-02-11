bd17515cdc516eafa8065812180d347ac55ebd6f
enti <enti@enti.enti>
2022-02-11 14:26:13 +0100

    update2

--- index.js
+++ index.js
@@ -3,25 +3,17 @@
 let http = require("http");
 let fs = require("fs");
 let http_server = http.createServer(function(req, res){
+	var archivo = ("index.html");
+	if (req.url != "/" ){
+		archivo = req.url.substring(1);
+	}
 
-if (req.url == "/character.png");{
-	fs.readFile("character.png", function (err,data){
+	fs.readFile(archivo, function(err, data){
 	if (err){
-	console.log("Error");
-	return
-}
-res.writeHead(200);
-res.end(data);
-});
-return
-
-}
-fs.readFile("index.html", function(err, data){
-if (err){
 
 		console.log("Error"); 
 		return
-		}		
+	}		
 	res.writeHead(200);
 	res.end(data);
 });
